48ec6761b1f729feedf70875b8743aa35661a3c8
ZLAA <72907523+zlaazlaa@users.noreply.github.com>
2022-10-27 17:28:00 +0800

    Add files via upload

--- out/html/search/searchdata.js
+++ out/html/search/searchdata.js
@@ -1,21 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "cstu",
-  1: "u",
-  2: "cst"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "files",
-  2: "functions"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Files",
-  2: "Functions"
 };
 
